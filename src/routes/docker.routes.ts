00434ae7099a64921dbb581f89cdedcad1dd2825
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-02-16 21:42:02 +0530

    initiated docker service router
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

---
+++ src/routes/docker.routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+import {  } from "../controllers";
+
+export const dockerServiceRouter = Router();
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+
 import { authRouter } from "./auth.routes";
+import { dockerServiceRouter } from "./docker.routes";
 
 export const rootRouter = Router();
 
 rootRouter.use("/auth", authRouter);
+rootRouter.use("/docker-service", dockerServiceRouter)
